5d19d0429948bd521faccf396e11214d00871297
AMAN KHAN <124271834+TAIHAN86@users.noreply.github.com>
2023-10-18 14:04:31 +0530

    added files

---
+++ my-app/src/views/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Home.css'
+
+function Home(){
+    return (
+        <div>
+            <h1 className='app-title'>weather app</h1>
+        </div>
+    )
+}
+export default Home 
